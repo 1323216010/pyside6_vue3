c9628c4fd0d2b82524e67593c8706c8e523400df
Pengcheng.yan <Pengcheng.yan@cowellchina.com>
2023-10-09 15:45:19 +0800

    修改描述

--- vue3/src/components/HelloWorld.vue
+++ vue3/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>Welcome to Your Vue.js App</h1>
+    <h1>Welcome to PDF WorkToolBox App</h1>
     <button @click="sendMessage">Send Message to Python</button>
     <div id="app">{{ logAckMsg }}</div>
   </div>
